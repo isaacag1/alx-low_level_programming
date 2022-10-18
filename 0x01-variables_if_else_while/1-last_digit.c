cbcc19741f397c5ff4869eae51a26128ad533100
litocs25 <113814098+litocs25@users.noreply.github.com>
2022-10-17 20:36:23 -0400

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
-	
 	if (x > 5)
 	{
 		printf("Last digit of %d is %d, and is greater than 5\n", n, x);
-	}	       
+	}
 	else if (x == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, x);
