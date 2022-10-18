7df770875bb491b7678809e965aad53599a838e1
litocs25 <113814098+litocs25@users.noreply.github.com>
2022-10-17 21:40:41 -0400

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - prints alpha in lower case
  * Return: Always 0
  *
  */
-
 int main(void)
 {
-	char lc:	
+	int ch;	
+	
 	for (lc = 'a'; lc <= 'z'; lc++);
 	{
-		putchar(lc);
+	     putchar(ch);
 	}
 	putchar('\n');
-	return (0);
+return (0);
 }
