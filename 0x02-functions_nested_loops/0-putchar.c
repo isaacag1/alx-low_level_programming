a02d1afa8601ce8e3e8b6a70cc97dfac00e8dfb0
litocs25 <isaac.agyemang1@gmail.com>
2022-10-18 10:14:05 -0400

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
 
 int main(void)
 {
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	-putchar('\n');
+	return (0);
 }
