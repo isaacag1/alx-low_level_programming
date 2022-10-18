91297f892dfec9d7d74534a45b8ffafb60950a71
litocs25 <113814098+litocs25@users.noreply.github.com>
2022-10-17 22:04:44 -0400

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,10 +10,9 @@ int main(void)
 {
 	char ch;
 	
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-	     putchar(ch);
-	}
-	putchar('\n');
-return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)	
+	putchar(ch);
+	
+	putchar('\n'):
+	return (0);
 }
