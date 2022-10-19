2ae8813ae46708c6da730d9ba23b8370813e38d9
litocs25 <isaac.agyemang1@gmail.com>
2022-10-18 17:04:14 -0700

    prints the sign of a number

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char cx;
+
+	for (cx = 'a'; cx <= 'z'; cx++)
+		_putchar(cx);
+
+	_putchar('\n')
+}
